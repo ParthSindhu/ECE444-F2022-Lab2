7d310700249dbb907c8c2b7193c185997c6c5f09
Parth Sindhu <sindhu.parth@gmail.com>
2022-09-28 12:39:43 -0400

    removing .venv from git

--- hello.py
+++ hello.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<h1>Hello World!</h1>'
 
+@app.route("/")
+def index():
+    return "<h1>Hello World!</h1>"
+
+
+@app.route("/user/<name>")
+def user(name):
+    return "<h1>Hello, {}!</h1>".format(name)
